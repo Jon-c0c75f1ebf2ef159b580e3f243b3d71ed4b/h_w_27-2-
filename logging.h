362df201400f72dc62caf61afb9130f5f7254d54
Jon <148043487+Jon-c0c75f1ebf2ef159b580e3f243b3d71ed4b@users.noreply.github.com>
2024-06-27 20:11:00 +0300

    Update logging.h

--- logging.h
+++ logging.h
@@ -1,59 +1,18 @@
 #pragma once
 #include <string>
 #include <fstream>
+#include <iostream>
 
 class Logging
 {
 public:
-	Logging()
-	{
-		_file = std::fstream("log.txt", std::ios::out | std::ios::app); //std::ios::in |
+	Logging();
 
-		if (!_file) {
-			// Для создания файла используем параметр ios::trunc
-			_file = std::fstream("log.txt", std::ios::in | std::ios::out | std::ios::trunc);
-		}
-	}
+	~Logging();
 
-	~Logging() {
-		_file.close();
-	}
+	void record(const std::string& sender, const std::string& recipient, const std::string& message);
 
-	void record(const std::string& sender, const std::string& recipient, const std::string& message) {
-
-		if (_file) {
-			_file << sender + " " + recipient + " " + message + "\n";
-			_file.close();
-		}
-		else {
-			cout << "eror logging\n";
-		}
-
-	}
-
-	void reading() {
-		_file = std::fstream("log.txt", std::ios::in); // | std::ios::app ; ate - переходите к концу потока сразу после открытия
-
-		if (!_file) {
-			cout << "error reading log\n";
-			return;
-		}
-		_file.seekg(std::ios_base::beg);
-		if (_file.eof()) {
-			return;
-		}
-		char ch = 'a';
-		int exit = 10000; //freeze protection
-		while (exit) {
-			_file.get(ch);
-			if (ch == '\n' || _file.eof()	) {
-				cout << '\n'; 
-				return;
-			}
-			cout << ch;
-			--exit;
-		}
-	}
+	void reading();
 
 private:
 	std::fstream _file;
